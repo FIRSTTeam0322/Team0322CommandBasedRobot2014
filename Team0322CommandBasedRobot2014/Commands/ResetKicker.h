21f86f92ce5917ddd06550cc24bb453ac6a2a683
Raa'Shaun Hunter <RaaShaun.Hunter@Gmail.com>
2014-02-13 16:09:42 -0500

    ResetKicker CommandGroup
    This CommandGroup combines the ReelInKicker and ResetRollers Commands to
    prepare the Kicker to fire.

---
+++ Team0322CommandBasedRobot2014/Commands/ResetKicker.h
@@ -0,0 +1,16 @@
+#ifndef RESET_KICKER_H
+#define RESET_KICKER_H
+
+#include "Commands/CommandGroup.h"
+
+/**
+ *
+ *
+ * @author CCRLH85
+ */
+class ResetKicker: public CommandGroup {
+public:	
+	ResetKicker();
+};
+
+#endif
